52a0910d3250a9c45efe04edd99478ae31d81bd7
Michel <mc.ubilla93@gmail.com>
2020-07-07 22:45:41 -0400

    upgrade

---
+++ clase.rb
@@ -0,0 +1,16 @@
+class Punto
+    attr_reader :x, :y
+    def initialize(x,y)
+        @x = x
+        @y = y
+    end
+
+    def avanzar
+        @y += 1
+
+    end
+end
+
+p1 = Punto.new(2,3)
+puts p1.avanzar
+
